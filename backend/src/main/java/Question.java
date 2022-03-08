c362f0c98564dd7c071aec6ce4977a9be657c643
samizoubi <91300834+samizoubi@users.noreply.github.com>
2022-03-07 22:53:19 -0500

    Add files via upload

---
+++ backend/src/main/java/Question.java
@@ -0,0 +1,48 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*
+ * A class to represent all questions
+ */
+
+@Entity
+public abstract class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected long id;
+    protected String question;
+
+
+    public Question() {
+    }
+
+
+    public Question(String question) {
+        this.question = question;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public String getQuestion() {
+        return question;
+    }
+
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+}
+
