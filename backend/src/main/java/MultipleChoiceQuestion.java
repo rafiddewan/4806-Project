2ed0d04e100aeb9bda47fc73db67986f8b22889f
samizoubi <91300834+samizoubi@users.noreply.github.com>
2022-03-07 23:07:51 -0500

    Add files via upload

---
+++ backend/src/main/java/MultipleChoiceQuestion.java
@@ -0,0 +1,62 @@
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * A question type that allows selecting one option from a dropdown of many
+ */
+
+@Entity
+public class MultipleChoiceQuestion extends Question {
+
+
+    private List<String> options;
+    private List<String> answers = new ArrayList<>();
+
+    /**
+     * Default constructor
+     */
+    public MultipleChoiceQuestion() {
+        super();
+    }
+
+    /**
+     * Constructor with specified question
+     * @param question the question text
+     * @param options the possible options to choose from
+     */
+    public MultipleChoiceQuestion(String question, ArrayList<String> options) {
+
+        super(question);
+        this.options = options;
+
+    }
+
+    public Collection<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(ArrayList<String> options) {
+        this.options = options;
+    }
+
+    public Collection<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * Adds a new answer
+     * @param answer String
+     */
+    public void addAnswer(String answer) {
+        this.answers.add(answer);
+    }
+}
