ce400404430bff9c2ce605b479009f4d84c44f37
Rafid Dewan <rafid.dewan145@gmail.com>
2022-03-09 10:09:27 -0500

    Created initial test case for ci/cd deployment

---
+++ Mini-SurveyMonkey/src/test/java/survey/TestWebApp.java
@@ -0,0 +1,46 @@
+package survey;
+
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import survey.Survey;
+import survey.SurveyRepository;
+import survey.WebController;
+
+@WebMvcTest(WebController.class)
+public class TestWebApp {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SurveyRepository service;
+
+    @Test
+    public void testSurvey() throws Exception {
+        Survey survey = new Survey();
+        survey.setId(1);
+        when(service.findById(1)).thenReturn(survey);
+        when(service.save(ArgumentMatchers.any())).thenReturn(survey);
+
+        this.mockMvc.perform(post("/CreateSurvey")).andExpect(MockMvcResultMatchers.status().isOk());
+        this.mockMvc.perform(get("/Survey")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("{\"id\":1,\"name\":null,\"questions\":[]}")));
+    }
+
+
+}
