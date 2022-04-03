2e14429f13fddab37418ab4d5f70818c4e527c92
sarashikh <sarasshikh@gmail.com>
2022-04-03 10:58:54 -0400

    D3 task
    Handling submission and validation.

---
+++ src/main/resources/static/listAllSurveys.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  getSurveys();
+});
+
+function getSurveys() {
+  //Handle GET request with ajax
+  $.ajax({
+    type: "GET",
+    url: "http://127.0.0.1:8080/surveys",
+  })
+    .done(function (data) {
+      displaySurveys(data);
+    })
+    .fail(function (jqXHR, textStatus) {
+      alert("FAILED REQUEST. Please try again");
+    });
+}
+
+function displaySurveys(data) {
+  //alert(data)
+  var html = "";
+  html +=
+    "<table class='center styled-table'><thead><th>ID</th> <th>Survey Name</th><th>Start?</th></thead>";
+  for (let i = 0; i < data.length; i++) {
+    html += createTableRow(data[i]);
+  }
+  html += "</table>";
+  $("#surveys").append(html);
+}
+
+function createTableRow(survey) {
+  var id = survey.id;
+  var name = survey.name;
+  var isOpen = survey.open;
+  var html = `<tr><td>${id}</td><td>${name}</td><td>`;
+  if (isOpen === true) {
+    html += `<button type='button' id="${id}" class='btn btn-outline-success px-4 mt-1' onclick="startSurvey(this)">Start</button>`;
+  } else {
+    html += "<p>Closed</p>";
+  }
+  html += "</td></tr>";
+
+  return html;
+}
+
+function startSurvey(element) {
+  surveyId = element.id;
+  //Handle POST request with ajax
+  window.location.href = `/user.html?surveyId=${surveyId}`;
+}
--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -1,15 +1,29 @@
+const url = new URL(window.location.href);
+const surveyId = url.searchParams.get("surveyId");
+let questionIdTypePair = new Map();
+
 $(document).ready(function () {
   $.ajax({
     type: "GET",
-    url: "/lastSurvey",
+    url: `http://127.0.0.1:8080/survey/${surveyId}`,
   })
     .done(function (data) {
+      data.questions.forEach((question) => {
+        if (question.questionType == "MULTIPLE_CHOICE") {
+          questionIdTypePair.set(question.id, "mcq");
+        } else if (question.questionType == "NUMERICAL_RANGE") {
+          questionIdTypePair.set(question.id, "numerical");
+        } else {
+          questionIdTypePair.set(question.id, "openEnded");
+        }
+      });
       generateSurveyForm(data);
+      addFormListener();
     })
     .fail(function (jqXHR, textStatus) {
-        $("div.surveyForm").html(function () {
-            return `<h1>Error: GET request failed</h1>`;
-        });
+      $("div.surveyForm").html(function () {
+        return `<h1>Error: GET request failed</h1>`;
+      });
     });
 });
 
@@ -18,7 +32,7 @@ function generateSurveyForm(data) {
     htmlString = "";
     htmlString += `<h1 class='display-5 text-center mt-3 pt-3'>${data.name}</h1>`;
     htmlString += `
-        <form action="index.html" method "POST" onsubmit="alert('you submitted the form');">
+        <form id="myForm">
             <div class="container">
                 <div class="row justify-content-center">`;
 
@@ -28,7 +42,7 @@ function generateSurveyForm(data) {
           htmlString += `
             <div class="col-sm-8 p-5 rounded">
                 <p><strong>${question.question}</strong></p>
-                <textarea class="form-control" id="id2" rows="3"></textarea>
+                <textarea class="form-control" id="${question.id}" rows="3" name="${question.id}" required></textarea>
             </div>`;
           break;
 
@@ -36,10 +50,11 @@ function generateSurveyForm(data) {
           optionsHtml = "";
           const keys = Object.keys(question.options);
           keys.forEach((key) => {
+            let id = key.replace(/\s/g, "-");
             optionsHtml += `
                 <div class="form-check mb-2">
-                    <input class="form-check-input" type="radio" name="exampleForm" id="radioExample2" />
-                    <label class="form-check-label" for="radioExample2">
+                    <input class="form-check-input" type="radio" name="${question.id}" id="${id}" value="${key}" required/>
+                    <label class="form-check-label" for="${id}">
                         ${key}
                     </label>
                 </div>`;
@@ -56,7 +71,7 @@ function generateSurveyForm(data) {
             <div class="col-sm-8 p-5 rounded">
                 <p><strong>${question.question}</strong></p>
                 <div>
-                    <input type="text" class="form-control" id="id3" placeholder="Enter Number">
+                    <input type="number" min="${question.lowerBound}" max="${question.upperBound}" class="form-control" id="${question.id}" name="${question.id}" placeholder="Enter Number" required>
                 </div>
                 <small class="form-label">Range: ${question.lowerBound} - ${question.upperBound}</small>
             </div>`;
@@ -74,7 +89,47 @@ function generateSurveyForm(data) {
             </div>
         </div>
     </form>`;
-    
+
     return htmlString;
+  });
+}
+
+function addFormListener() {
+  $("#myForm").submit(function (e) {
+    //prevent Default functionality
+    e.preventDefault();
+
+    let formData = $("#myForm").serializeArray();
+
+    console.log(formData);
+    console.log(questionIdTypePair);
+
+    let error = false;
+    let success = true;
+    formData.forEach((data) => {
+      let question_type = questionIdTypePair.get(parseInt(data.name));
+      console.log(question_type);
+      $.ajax({
+        type: "PATCH",
+        url: `/survey/${question_type}/${data.name}/submit`,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        data: `{"answer": "${data.value}"}`,
+      })
+        .done(function (data) {
+          console.log("Created POST request for question");
+        })
+        .fail(function (jqXHR, textStatus) {
+          error = true;
+        });
+      if (error) {
+        alert("Submit failed");
+      } else {
+        alert("Submitted Successfully!");
+        window.location.href = "/";
+      }
+    });
   });
 }
